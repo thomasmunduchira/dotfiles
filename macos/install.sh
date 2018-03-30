991b89a6bc09f56b487c6ca62e89209bd2707ead
Thomas Munduchira <thomas@thomasmunduchira.com>
2018-03-29 23:44:50 -0700

    Reorganize install.sh

--- macos/install.sh
+++ macos/install.sh
@@ -1,8 +1,10 @@
-# Dont continue on error
+# Don't continue on error
 set -e
 
+# Go to parent directory
 cd ..
 
+# Define constants
 ZSH_THEME="guri.zsh-theme"
 
 CURRENT_PATH=$(pwd)
@@ -23,8 +25,6 @@ ZSH_FILES=("zshrc")
 GIT_FILES=("gitignore")
 VIM_FILES=("vimrc")
 
-CONFIG_SCRIPTS=("os" "chrome")
-
 command_exists() {
   type "$1" &>/dev/null
 }
@@ -34,9 +34,17 @@ symlink_multiple() {
   files="$2"
 
   for file in $files; do
-    echo "    Linking .$file!"
     ln -sf "$CURRENT_PATH/$source_dir/$file" "$HOME/.$file"
   done
+}
+
+install_fonts() {
+  echo "    Installing Powerline fonts!"
+  git clone https://github.com/powerline/fonts.git --depth=1
+  cd fonts
+  ./install.sh
+  cd ..
+  rm -rf fonts
 }
 
 install_brew() {
@@ -44,11 +52,22 @@ install_brew() {
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 }
 
+upgrade_brew() {
+  echo "    Upgrading and cleaning up brew formulae!"
+  brew upgrade
+  brew cleanup
+}
+
 install_bash_it() {
   echo "    Installing bash_it!"
   rm -rf "$BASH_IT_CONFIG"
   git clone --depth=1 https://github.com/Bash-it/bash-it.git "$BASH_IT_CONFIG"
   "$BASH_IT_CONFIG/install.sh" --no-modify-config
+}
+
+configure_bash() {
+  echo "    Configuring bash!"
+  symlink_multiple $BASH_DIR $BASH_FILES
 }
 
 install_zsh() {
@@ -62,21 +81,26 @@ install_oh_my_zsh() {
   curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 }
 
-load_oh_my_zsh_theme() {
-  echo "    Loading Oh My Zsh theme"
+configure_zsh() {
+  echo "    Configuring zsh!"
+  symlink_multiple $ZSH_DIR $ZSH_FILES
   ln -sf "$CURRENT_PATH/$ZSH_DIR/$ZSH_THEME" "$OH_MY_ZSH_CONFIG/themes/$ZSH_THEME"
-}
-
-install_powerline_fonts() {
-  git clone https://github.com/powerline/fonts.git --depth=1
-  cd fonts
-  ./install.sh
-  cd ..
-  rm -rf fonts
-}
-
-install_zsh_syntax_highlighting() {
   brew install zsh-syntax-highlighting
+}
+
+install_git() {
+  echo "    Installing git!"
+  brew install git
+}
+
+configure_git() {
+  echo "    Configuring git!"
+  symlink_multiple $GIT_DIR $GIT_FILES
+}
+
+install_vim() {
+  echo "    Installing vim!"
+  brew install vim
 }
 
 install_nvim() {
@@ -84,18 +108,28 @@ install_nvim() {
   brew install neovim
 }
 
-configure_nvim() {
-  echo "   Linking init.vim with vimrc!"
-  ln -sf "$CURRENT_PATH/$ZSH_DIR/$ZSH_THEME" "$ZSH_DIR/init.vim"
+install_vim_plug() {
+  echo "    Installing Plug for vim!"
+  curl -fLo "$VIM_CONFIG"/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 }
 
-install_plug() {
-  echo "    Installing Plug!"
-  curl -fLo "$VIM_CONFIG"/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+install_neovim_plug() {
+  echo "    Installing Plug for neovim!"
   curl -fLo "$NEOVIM_LOCAL"/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 }
 
+configure_vim() {
+  echo "    Configuring vim!"
+  symlink_multiple $VIM_DIR $VIM_FILES
+}
+
+configure_nvim() {
+  echo "    Configuring neovim!"
+  ln -sf "$CURRENT_PATH/$ZSH_DIR/$ZSH_THEME" "$ZSH_DIR/init.vim"
+}
+
 install_tmux() {
+  echo "    Installing tmux!"
   brew install tmux
 }
 
@@ -104,49 +138,43 @@ if ! command_exists curl; then
   exit 3
 fi
 
-echo "[ Homebrew ]"
+echo "[ Fonts ]"
+install_fonts
 
+echo "[ Homebrew ]"
 if ! command_exists brew; then
   install_brew
 fi
+upgrade_brew
 
 echo "[ Bash ]"
-
 mkdir -p "$BASH_IT_CONFIG"
-
 install_bash_it
-symlink_multiple $BASH_DIR $BASH_FILES
+configure_bash
 
 echo "[ ZSH ]"
-
-if ! command_exists zsh; then
-  install_zsh
-fi
-
+install_zsh
 mkdir -p "$OH_MY_ZSH_CONFIG/themes"
-
 install_oh_my_zsh
-symlink_multiple $ZSH_DIR $ZSH_FILES
-load_oh_my_zsh_theme
-install_powerline_fonts
-install_zsh_syntax_highlighting
+configure_zsh
 
 echo "[ Git ]"
+install_git
+configure_git
 
-symlink_multiple $GIT_DIR $GIT_FILES
-
-echo "[ Vim/Neovim ]"
-
-if ! command_exists nvim; then
-  install_nvim
-fi
-
+echo "[ Vim ]"
+install_vim
 mkdir -p "$VIM_CONFIG/autoload"
+install_vim_plug
+configure_vim
+
+echo "[ Neovim ]"
+install_nvim
 mkdir -p "$NEOVIM_LOCAL/autoload"
 mkdir -p "$NEOVIM_CONFIG"
-
-install_plug
-symlink_multiple $VIM_DIR $VIM_FILES
+install_neovim_plug
 configure_nvim
 
+echo "[ tmux ]"
 install_tmux
+
